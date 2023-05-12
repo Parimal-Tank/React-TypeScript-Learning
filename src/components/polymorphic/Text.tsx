4ecf8bead1143dc4b4348be3defc361be3497026
parimalt <parimalt@zignuts.com>
2023-05-12 18:34:25 +0530

    Bug Fix

--- src/components/polymorphic/Text.tsx
+++ src/components/polymorphic/Text.tsx
@@ -1,21 +1,19 @@
-import React, { Children } from 'react'
+import React from 'react'
 
-type TextOwnProps<E extends React.ElementType> = {
+type TextOwnProps = {
     size?: 'sm' | 'md' | 'lg'
     color?: 'primary' | 'secondary'
     children : React.ReactNode
     as?: React.ElementType
 }
 
-type TextProps<E extends React.ElementType> = TextOwnProps<E> &  Omit<React.ComponentProps<E> , keyof TextOwnProps<E>>
-
-const Text = <E extends React.ElementType = 'div'>({ size , color , children ,as }: TextProps<E>) => {
+const Text = ({ size , color , children ,as }: TextOwnProps) => {
 
     const Component = as || 'div';
 
   return (
     <Component className={`class-with-${size}-${color}`}>
-       {Children}
+       {children}
     </Component>
   )
 }
